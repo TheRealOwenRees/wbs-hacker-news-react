3496644e3bbab883ba6005ae960245211814d4cd
Owen Rees <owenrees82@gmail.com>
2023-03-01 13:52:29 +0100

    dynamic search and results

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,29 @@
+import { useState } from "react";
+
 const Search = ( {entries, setEntries }) => {
 
+  const [searchText, setSearchText] = useState()
+
   const handleSearch = () => {
-    console.log("search fired")
-    fetch('http://hn.algolia.com/api/v1/search?query=react&page=1')
+    console.log(`search fired for ${searchText}`)
+    fetch(`http://hn.algolia.com/api/v1/search?query=${searchText}`)
     .then(response => response.json())
     .then(data => setEntries(data.hits)); 
   }
 
-    return (
-      <div className='search' id='search'>
-        <h1>Header</h1>
-        <input type='text' />
-        <button type='button' onClick={handleSearch}>
-          Submit
-        </button>
-      </div>
-    );
-  };
+  return (
+    <div className='search' id='search'>
+      <h1>Header</h1>
+      <input 
+        type='text' 
+        value={searchText} 
+        onChange={(e) => setSearchText(e.target.value)}/>
+      <button type='button' onClick={handleSearch}>
+        Submit
+      </button>
+    </div>
+  );
+};
   
   export default Search;
   
