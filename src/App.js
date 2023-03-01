7fdefa750018e8f0632004caca14862f3a012c49
KONSTRUKTEUR <raumerkunder@gmail.com>
2023-03-01 20:55:42 +0100

    Add 'Style', keyboard interaction, etc.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import './App.css';
-import Search from './components/Search'
-import Entry from './components/Entry'
-import { useEffect, useState } from 'react';
-import loading from './images/loading.gif'
+import Search from "./components/Search";
+import Entry from "./components/Entry";
+import { useEffect, useState } from "react";
+import loading from "./images/loading.gif";
 
 function App() {
-
   const [entries, setEntries] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
-    setIsLoading(false)
-  }, [entries])
+    setIsLoading(false);
+  }, [entries]);
+
+  const handlePagination = () => {};
 
   return (
     <div>
-      <h1>Hacker News Search</h1>
-      <Search entries={entries} setEntries={setEntries} setIsLoading={setIsLoading} />
-      {isLoading === true && <img src={loading} alt="loading"></img>}
-      {entries.length > 1
-      ? entries.map((entry) => <Entry key={entry.objectID} entry={entry} setIsLoading={setIsLoading} />)
-      : <p>No results were found</p>}
+      <Search
+        entries={entries}
+        setEntries={setEntries}
+        setIsLoading={setIsLoading}
+      />
+      <div>
+        {entries.length > 0
+          ? Array.from({ length: 50 }, (_, i) => (
+              <div className='pageLink' key={i} onClick={handlePagination}>
+                {i}
+              </div>
+            ))
+          : "huhu"}
+      </div>
+      <div>
+        {isLoading === true && <img src={loading} alt='loading'></img>}
+        {entries.length > 1 ? (
+          entries.map((entry) => (
+            <Entry
+              key={entry.objectID}
+              entry={entry}
+              setIsLoading={setIsLoading}
+            />
+          ))
+        ) : (
+          <p>No results were found</p>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,10 +1,19 @@
-const Entry = ({ entry }) => {
-    return (
-      <div className='search'>
-        <a href={entry.url} target="_blank" rel="noreferrer">{entry.title}</a>
+const Entry = ({ entry, setIsLoading }) => {
+  return (
+    <div className='result-entry' key={entry.index}>
+      <div className='entry-title'>
+        {setIsLoading(false)}
+        {entry.title}
       </div>
-    );
-  };
-  
+      <div className='entry-link'>
+        <div className='entry-points'>{entry.points}</div>
+        <div className='entry-author'>{entry.author}</div>
+        <a href={entry.url} target='_blank'>
+          {entry.url}
+        </a>
+      </div>
+    </div>
+  );
+};
+
 export default Entry;
-  
